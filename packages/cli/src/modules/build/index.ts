6e0e59419427df497101c4e024dd5fdc8cf2833d
aramissennyeydd <aramis.sennyey@doordash.com>
2025-05-13 18:04:57 -0400

    feat: promote backstage-cli-alpha to the main entrypoint
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>

--- packages/cli/src/modules/build/index.ts
+++ packages/cli/src/modules/build/index.ts
@@ -13,71 +13,103 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Command, Option } from 'commander';
+import { createCliPlugin } from '../../wiring/factory';
 import { lazy } from '../../lib/lazy';
+import { registerPackageCommands } from '.';
 
-export function registerRepoCommands(command: Command) {
-  command
-    .command('build')
-    .description(
-      'Build packages in the project, excluding bundled app and backend packages.',
-    )
-    .option(
-      '--all',
-      'Build all packages, including bundled app and backend packages.',
-    )
-    .option(
-      '--since <ref>',
-      'Only build packages and their dev dependents that changed since the specified ref',
-    )
-    .option(
-      '--minify',
-      'Minify the generated code. Does not apply to app package (app is minified by default).',
-    )
-    .action(lazy(() => import('./commands/repo/build'), 'command'));
-}
+export const buildPlugin = createCliPlugin({
+  pluginId: 'build',
+  init: async reg => {
+    reg.addCommand({
+      path: ['package', 'build'],
+      description: 'Build a package for production deployment or publishing',
+      execute: async ({ args }) => {
+        const command = new Command();
 
-export function registerPackageCommands(command: Command) {
-  command
-    .command('build')
-    .description('Build a package for production deployment or publishing')
-    .option('--role <name>', 'Run the command with an explicit package role')
-    .option(
-      '--minify',
-      'Minify the generated code. Does not apply to app package (app is minified by default).',
-    )
-    .option(
-      '--skip-build-dependencies',
-      'Skip the automatic building of local dependencies. Applies to backend packages only.',
-    )
-    .option(
-      '--stats',
-      'If bundle stats are available, write them to the output directory. Applies to app packages only.',
-    )
-    .option(
-      '--config <path>',
-      'Config files to load instead of app-config.yaml. Applies to app packages only.',
-      (opt: string, opts: string[]) => (opts ? [...opts, opt] : [opt]),
-      Array<string>(),
-    )
-    .action(lazy(() => import('./commands/package/build'), 'command'));
-}
+        const defaultCommand = command
+          .option(
+            '--role <name>',
+            'Run the command with an explicit package role',
+          )
+          .option(
+            '--minify',
+            'Minify the generated code. Does not apply to app package (app is minified by default).',
+          )
+          .option(
+            '--skip-build-dependencies',
+            'Skip the automatic building of local dependencies. Applies to backend packages only.',
+          )
+          .option(
+            '--stats',
+            'If bundle stats are available, write them to the output directory. Applies to app packages only.',
+          )
+          .option(
+            '--config <path>',
+            'Config files to load instead of app-config.yaml. Applies to app packages only.',
+            (opt: string, opts: string[]) => (opts ? [...opts, opt] : [opt]),
+            Array<string>(),
+          )
+          .action(lazy(() => import('./commands/package/build'), 'command'));
+        await defaultCommand.parseAsync(args, { from: 'user' });
+      },
+    });
 
-export function registerCommands(program: Command) {
-  program
-    .command('build-workspace <workspace-dir> [packages...]')
-    .addOption(
-      new Option(
-        '--alwaysYarnPack',
-        'Alias for --alwaysPack for backwards compatibility.',
-      )
-        .implies({ alwaysPack: true })
-        .hideHelp(true),
-    )
-    .option(
-      '--alwaysPack',
-      'Force workspace output to be a result of running `yarn pack` on each package (warning: very slow)',
-    )
-    .description('Builds a temporary dist workspace from the provided packages')
-    .action(lazy(() => import('./commands/buildWorkspace'), 'default'));
-}
+    reg.addCommand({
+      path: ['repo', 'build'],
+      description:
+        'Build packages in the project, excluding bundled app and backend packages.',
+      execute: async ({ args }) => {
+        const command = new Command();
+
+        // This command expect `package build` to be registered, as its used to parse
+        //  individual plugins' package build scripts.
+        registerPackageCommands(command.command('package'));
+
+        const defaultCommand = command
+          .option(
+            '--all',
+            'Build all packages, including bundled app and backend packages.',
+          )
+          .option(
+            '--since <ref>',
+            'Only build packages and their dev dependents that changed since the specified ref',
+          )
+          .option(
+            '--minify',
+            'Minify the generated code. Does not apply to app package (app is minified by default).',
+          )
+          .action(lazy(() => import('./commands/repo/build'), 'command'));
+        await defaultCommand.parseAsync(args, { from: 'user' });
+      },
+    });
+
+    reg.addCommand({
+      path: ['build-workspace'],
+      description:
+        'Builds a temporary dist workspace from the provided packages',
+      execute: async ({ args }) => {
+        const command = new Command();
+        const defaultCommand = command
+          .arguments('<workspace-dir> [packages...]')
+          .addOption(
+            new Option(
+              '--alwaysYarnPack',
+              'Alias for --alwaysPack for backwards compatibility.',
+            )
+              .implies({ alwaysPack: true })
+              .hideHelp(true),
+          )
+          .option(
+            '--alwaysPack',
+            'Force workspace output to be a result of running `yarn pack` on each package (warning: very slow)',
+          )
+          .action(lazy(() => import('./commands/buildWorkspace'), 'default'));
+        await defaultCommand.parseAsync(args, { from: 'user' });
+      },
+    });
+  },
+});
+
+export default buildPlugin;
--- packages/cli/src/modules/start/index.ts
+++ packages/cli/src/modules/start/index.ts
@@ -13,57 +13,87 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { Command } from 'commander';
+import { createCliPlugin } from '../../wiring/factory';
 import { lazy } from '../../lib/lazy';
 import { configOption } from '../config';
 
-export function registerRepoCommands(command: Command) {
-  command
-    .command('start')
-    .description('Starts packages in the repo for local development')
-    .argument(
-      '[packageNameOrPath...]',
-      'Run the specified package instead of the defaults.',
-    )
-    .option(
-      '--plugin <pluginId>',
-      'Start the dev entry-point for any matching plugin package in the repo',
-      (opt: string, opts: string[]) => (opts ? [...opts, opt] : [opt]),
-      Array<string>(),
-    )
-    .option(...configOption)
-    .option(
-      '--inspect [host]',
-      'Enable debugger in Node.js environments. Applies to backend package only',
-    )
-    .option(
-      '--inspect-brk [host]',
-      'Enable debugger in Node.js environments, breaking before code starts. Applies to backend package only',
-    )
-    .option(
-      '--require <path...>',
-      'Add a --require argument to the node process. Applies to backend package only',
-    )
-    .option('--link <path>', 'Link an external workspace for module resolution')
-    .action(lazy(() => import('./commands/repo/start'), 'command'));
-}
+export const startPlugin = createCliPlugin({
+  pluginId: 'start',
+  init: async reg => {
+    reg.addCommand({
+      path: ['package', 'start'],
+      description: 'Start a package for local development',
+      execute: async ({ args }) => {
+        const command = new Command();
 
-export function registerPackageCommands(command: Command) {
-  command
-    .command('start')
-    .description('Start a package for local development')
-    .option(...configOption)
-    .option('--role <name>', 'Run the command with an explicit package role')
-    .option('--check', 'Enable type checking and linting if available')
-    .option('--inspect [host]', 'Enable debugger in Node.js environments')
-    .option(
-      '--inspect-brk [host]',
-      'Enable debugger in Node.js environments, breaking before code starts',
-    )
-    .option(
-      '--require <path...>',
-      'Add a --require argument to the node process',
-    )
-    .option('--link <path>', 'Link an external workspace for module resolution')
-    .action(lazy(() => import('./commands/package/start'), 'command'));
-}
+        const defaultCommand = command
+          .option(...configOption)
+          .option(
+            '--role <name>',
+            'Run the command with an explicit package role',
+          )
+          .option('--check', 'Enable type checking and linting if available')
+          .option('--inspect [host]', 'Enable debugger in Node.js environments')
+          .option(
+            '--inspect-brk [host]',
+            'Enable debugger in Node.js environments, breaking before code starts',
+          )
+          .option(
+            '--require <path...>',
+            'Add a --require argument to the node process',
+          )
+          .option(
+            '--link <path>',
+            'Link an external workspace for module resolution',
+          )
+          .action(lazy(() => import('./commands/package/start'), 'command'));
+
+        await defaultCommand.parseAsync(args, { from: 'user' });
+      },
+    });
+
+    reg.addCommand({
+      path: ['repo', 'start'],
+      description: 'Starts packages in the repo for local development',
+      execute: async ({ args }) => {
+        const command = new Command();
+
+        const defaultCommand = command
+          .argument(
+            '[...packageNameOrPath]',
+            'Run the specified package instead of the defaults.',
+          )
+          .option(
+            '--plugin <pluginId>',
+            'Start the dev entry-point for any matching plugin package in the repo',
+            (opt: string, opts: string[]) => (opts ? [...opts, opt] : [opt]),
+            Array<string>(),
+          )
+          .option(...configOption)
+          .option(
+            '--inspect [host]',
+            'Enable debugger in Node.js environments. Applies to backend package only',
+          )
+          .option(
+            '--inspect-brk [host]',
+            'Enable debugger in Node.js environments, breaking before code starts. Applies to backend package only',
+          )
+          .option(
+            '--require <path...>',
+            'Add a --require argument to the node process. Applies to backend package only',
+          )
+          .option(
+            '--link <path>',
+            'Link an external workspace for module resolution',
+          )
+          .action(lazy(() => import('./commands/repo/start'), 'command'));
+
+        await defaultCommand.parseAsync(args, { from: 'user' });
+      },
+    });
+  },
+});
+
+export default startPlugin;
